917e22d84485f4821f38351d50fa5f7af12955e3
gundariamborosati <gundariamborosati1998@gmail.com>
2018-02-23 17:31:31 +0700

    new
    new database

--- application/models/m_direktur.php
+++ application/models/m_direktur.php
@@ -1,17 +1,15 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-class m_logistik extends CI_Model {
+class m_direktur extends CI_Model {
 
-	function detail($username){
-		$this->db->where('username',$username);
-		return $this->db->get('user');
-	}
-
+	function __construct() {
+        parent::__construct();
+    }
+	
 	function cek($username, $password){
 		$this->db->where('username', $username);
 		$this->db->where('password', $password);
-		return $this->db->get('logistik');
+		return $this->db->get('direktur');
 	}
-
 }
