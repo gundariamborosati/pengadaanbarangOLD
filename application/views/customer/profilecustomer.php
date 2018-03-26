b14625970f5329edda004c180383709f7ec61f28
ayidhae <ayidhae@Gmail.com>
2018-03-26 10:55:10 +0700

    new
    new

--- application/views/customer/profilecustomer.php
+++ application/views/customer/profilecustomer.php
@@ -1,4 +1,4 @@
-<!DOCTYPE html>
+<!-- <!DOCTYPE html>
 <html lang="en">
 <head>
   <title>Bootstrap Example</title>
@@ -46,4 +46,134 @@
 </div>
 
 </body>
-</html>
+</html> -->
+
+<body class="app header-fixed sidebar-fixed aside-menu-fixed aside-menu-hidden">
+  <header class="app-header navbar">
+    <button class="navbar-toggler mobile-sidebar-toggler d-lg-none mr-auto" type="button">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <a class="navbar-brand" href="#"></a>
+    <button class="navbar-toggler sidebar-toggler d-md-down-none" type="button">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <style type="text/css">
+    body { font-family: sans-serif; }
+   </style>
+
+    <ul class="nav navbar-nav d-md-down-none">
+      <li class="nav-item px-3">
+        <a class="nav-link" href="<?php echo base_url('/c_customer/home');?>">Dashboard</a>
+      </li>
+    </ul>
+    <ul class="nav navbar-nav ml-auto">
+      <li class="nav-item dropdown">
+        <a class="nav-link" data-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false">
+          Welcome <?php echo $this->session->userdata('username');?>
+        </a>
+         <!--  <img src="" class="img-avatar" alt="Customer">
+        </a> -->
+        <div class="dropdown-menu dropdown-menu-right">
+          
+          
+          <div class="dropdown-header text-center">
+            <strong>Settings</strong>
+          </div>
+          <a class="dropdown-item" href="<?php echo base_url('c_customer/viewProfile'); ?>"><i class="fa fa-user"></i> Profile</a>
+          <a class="dropdown-item" href="<?php echo base_url('c_customer/update_profile'); ?>"><i class="fa fa-wrench"></i> Settings</a>
+          <a class="dropdown-item" href="<?php echo base_url('c_customer/keluar'); ?>"><i class="fa fa-lock"></i> Logout</a>
+        </div>
+      </li>
+    </ul>
+
+  </header>
+
+  <div class="app-body">
+    <div class="sidebar">
+      <nav class="sidebar-nav">
+        <ul class="nav">
+          <li class="nav-item">
+            <a class="nav-link" href="<?php echo base_url('/c_customer/home');?>"><i class="icon-speedometer"></i>customer Dashboard </a>
+          </li>
+
+        <li class="nav-title">
+            Menu
+          </li>
+
+          <li class="nav-item">  
+          <a class="nav-link" href=" <?php echo base_url('/c_customer/home');?> "><i class="fa fa-home"></i> Home</a>
+          </li>
+
+          <li class="nav-item nav-dropdown"> 
+          <a class="nav-link nav-dropdown-toggle" href="#"><i class="fa fa-file"></i> Kelola dokumen</a>          
+            <ul class="nav-dropdown-items">
+              <li class="nav-item">
+                <a class="nav-link" href="#"><i class="fa fa-share"></i> Send Dokumen</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#"><i class="fa fa-download"></i> Receive Dokumen</a>
+              </li> 
+            </ul>
+          </li>  
+           <li class="nav-item">
+            <a class="nav-link" href="widgets.html"><i class="fa fa-cart-arrow-down"></i> melihat status pesanan </a>
+            <a class="nav-link" href="widgets.html"><i class="fa fa-comment"></i> ulasan saya </a>
+          </li>
+          <li class="divider"></li>
+
+        </ul>
+      </nav>
+      <button class="sidebar-minimizer brand-minimizer" type="button"></button>
+    </div>
+
+    <!-- Main content -->
+     <main class="main"> 
+
+      <!-- Breadcrumb -->
+      <ol class="breadcrumb">
+        <li class="breadcrumb-item">Home</li>
+        <li class="breadcrumb-item"><a href="#">Customer</a></li>
+        <li class="breadcrumb-item active">Dashboard</li>
+
+        <!-- Breadcrumb Menu-->
+      </ol>
+    <?php foreach($profile as $user): ?>
+<div class="row">
+            <div class="col-lg-12">
+              <div class="card">
+                <div class="card-header">
+                  <i class="fa fa-user-o"></i> Profil perusahaan
+                </div>
+                <div class="card-body">
+                  <table class="table table-responsive-sm table-striped">   
+                    <thead>
+                      <tr>                      
+                        <th>Nama Perusahaan</th>
+                        <th>Alamat Perusahaan</th>
+                        <th>Email</th>
+                        <th>Contact</th>
+                        <th>Username</th>
+                      </tr>
+                    </thead>     
+                    <tbody>
+                      <tr>                      
+                        <td><?php echo $user['nama_perusahaan']; ?></td>
+                        <td><?php echo $user['alamat_perusahaan']; ?></td> 
+                        <td><?php echo $user['email']; ?></td> 
+                        <td><?php echo $user['contact']; ?></td> 
+                        <td><?php echo $user['username']; ?></td> 
+                      </tr>
+                    </tbody>                                      
+                  </table>
+                  <?php endforeach; ?>
+          </div>
+              </div>
+            </div>
+            <!--/.col-->
+          </div>
+      <!-- /.conainer-fluid -->
+    </main>
+
+
+
+  </div>
--- application/views/customer/updateprofile.php
+++ application/views/customer/updateprofile.php
@@ -1,29 +1,82 @@
-<div class="active tab-pane" id="settings">
-              <form class="form-horizontal" action="<?php echo base_url() . 'c_customer/update_profile/' . $this->session->userdata('username'); ?>" method="post">
-                <input type="hidden" name="txt_id" value="<?php echo $this->session->userdata('username');?>" class="form-control" placeholder="Nama login..." required>
-                <div class="form-group">
-                  <label for="namaLogin" class="col-sm-2 control-label">Nama perusahaan</label>
-                  <div class="col-sm-7">
-                    <input type="text" name="nama_perusahaan" value="<?php echo $this->session->userdata('nama_perusahaan');?>" class="form-control" placeholder="Nama perusahaan..." required>
-                  </div>
-                </div>
-                <div class="form-group">
-                  <label for="alamat_perusahaan" class="col-sm-2 control-label">alamat perusahaan</label>
-                  <div class="col-sm-7">
-                    <input type="text" name="alamat_perusahaan" value="<?php echo $this->session->userdata('alamat_perusahaan');?>" class="form-control"  placeholder="alamat..." required>
-                  </div>
-                </div>
-                 <div class="form-group">
-                  <label for="contact" class="col-sm-2 control-label">contact</label>
-                  <div class="col-sm-7">
-                    <input type="text" name="contact" value="<?php echo $this->session->userdata('contact');?>" class="form-control"  placeholder="contact..." required>
-                  </div>
-                </div>
-                <div class="form-group">
-                  <div class="col-sm-offset-2 col-sm-10">
-                    
-                    <button type="submit" class="btn btn-success"><i class="fa fa-check"></i> Simpan</button>
-                  </div>
-                </div>
-              </form>
+     <!-- <h1>Re</h1> -->
+            <!-- <p class="text-muted">Masukkan data-data perusahaan</p> -->
+            <!-- <?php echo form_open_multipart(base_url('c_customer/update_profile')) ;?>
+            
+            <?php echo validation_errors('<div class="alert alert-danger">','</div>'); ?>
+            <div class="input-group mb-3">
+              <div class="input-group-prepend">
+                <span class="input-group-text"><i class="fa fa-building"></i></span>
+              </div>
+               <input type="text" name="nama_perusahaan" value="<?php echo $this->session->userdata('nama_perusahaan');?>" class="form-control" placeholder="Nama perusahaan..." required>
             </div>
+            <div class="input-group mb-3">
+              <div class="input-group-prepend">
+                <span class="input-group-text"><i class="fa fa-map-marker"></i></span>
+              </div>
+              <input type="text" name="alamat_perusahaan" value="<?php echo $this->session->userdata('alamat_perusahaan');?>" class="form-control"  placeholder="alamat..." required>
+            </div>
+
+            <div class="input-group mb-3">
+              <div class="input-group-prepend">
+                <span class="input-group-text"><i class="fa fa-phone"></i></span>
+              </div>
+              <input type="text" name="contact" value="<?php echo $this->session->userdata('contact');?>" class="form-control"  placeholder="contact..." required>
+            </div>
+             <div class="input-group mb-3">
+              <div class="input-group-prepend">
+                <span class="input-group-text">@</span>
+              </div>
+              <input type="text" name="email" value="<?php echo $this->session->userdata('email');?>" class="form-control"  placeholder="email..." required>
+            </div>
+            <div class="input-group mb-3">
+              <div class="input-group-prepend">
+                <span class="input-group-text"><i class="icon-lock"></i></span>
+              </div>
+              <input id="password" type="password" name="password" onkeyup='check();' value="<?php echo $this->session->userdata('password');?>" class="form-control"  placeholder="email..." required>
+              <!-- <input id="password" type="password" class="form-control" placeholder="Password" name="password" onkeyup='check();' value="" required> -->
+            <!-- </div>
+
+            <div class="input-group mb-4">
+              <div class="input-group-prepend">
+                <span class="input-group-text"><i class="icon-lock"></i></span>
+              </div>
+              <input id="confirm_password" type="password" name="password" onkeyup='check();' value="<?php echo $this->session->userdata('password');?>" class="form-control"  placeholder="email..." required> -->
+            <!--   <input id="confirm_password" name="confirm_password" type="password" class="form-control" placeholder="Repeat password" onkeyup='check();' value="" required> -->
+          <!--     <span id='message'> </span>
+            </div>
+            <script> 
+            var check = function() {
+                if (document.getElementById('password').value ==
+                  document.getElementById('confirm_password').value && document.getElementById('password').value != '') {
+                  document.getElementById('message').style.color = 'green';
+                  document.getElementById('message').innerHTML = 'matching';
+                } else {
+                  document.getElementById('message').style.color = 'red';
+                  document.getElementById('message').innerHTML = 'not matching';
+                }
+              } 
+            </script>
+
+            <button type="submit" class="btn btn-success" class="fa fa-check">simpan</button>
+            </form>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+                
+            </div> 
+          </div>
+              </div>
+            </div> -->
+            <!--/.col-->
+         <!--  </div> -->
+      <!-- /.conainer-fluid -->
+    <!-- </main>
+  </div> -->
+   <!-- Bootstrap and necessary plugins -->
+  <!-- 
+  </body>
+  </main>
+  </div>
+  </body> --> -->
